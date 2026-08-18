e8ddf537c1b32088ddbffbc2651a1238bf139d31
Iahia <iahiaimon555@gmail.com>
2026-08-18 23:54:45 +0600

    random click added

--- tik_tak_toe.py
+++ tik_tak_toe.py
@@ -1,16 +1,51 @@
 from tkinter import *
 from tkinter.ttk import *
+import random
 
 
 class Gamebox:
     def __init__(self, master=None):
         self.master = master
         self.clicked_boxes = set()
+        self.computer_boxes = set()
         self.create()
 
-    def box_clicked(self, event, box, x1, y1, x2, y2):  
+    def create(self):
+        self.canvas = Canvas(self.master)
 
-        if box in self.clicked_boxes:
+        # 3 x 3 Grid
+        cell_size = 120
+        self.all_boxes = []
+        self.box_coordinates = {}
+        for row in range(3):
+            for col in range(3):
+                x1 = 70 + (col * cell_size)
+                y1 = 80 + (row * cell_size)
+
+                x2 = x1 + cell_size
+                y2 = y1 + cell_size
+
+
+
+                box = self.canvas.create_rectangle(
+                    x1, y1, x2, y2,
+                    outline="black",
+                    fill="white",
+                    width=2
+                )
+
+                self.all_boxes.append(box)
+                self.box_coordinates[box] = (x1, y1, x2, y2)
+
+
+                self.canvas.tag_bind(box, "<Button-1>", lambda event, box=box, x1=x1, y1=y1, x2=x2, y2=y2:
+        self.box_clicked(event, box, x1, y1, x2, y2))
+
+        self.canvas.pack(fill=BOTH, expand=1)
+
+    def box_clicked(self, event, box, x1, y1, x2, y2):
+
+        if box in self.clicked_boxes or box in self.computer_boxes:
             return
 
         self.clicked_boxes.add(box)
@@ -20,6 +55,7 @@ class Gamebox:
 
         size = 30
 
+        # User = X
         self.canvas.create_line(
             center_x - size, center_y - size,
             center_x + size, center_y + size,
@@ -32,28 +68,38 @@ class Gamebox:
             width=4
         )
 
-    def create(self):
-        self.canvas = Canvas(self.master)
+        # Computer-এর turn
+        self.computer_move()
 
-        # 3 x 3 Grid
-        cell_size = 120
+    def computer_move(self):
+        empty_boxes = [
+            box for box in self.all_boxes
+            if box not in self.clicked_boxes
+            and box not in self.computer_boxes
+        ]
 
-        for row in range(3):
-            for col in range(3):
-                x1 = 70 + (col * cell_size)
-                y1 = 80 + (row * cell_size)
+        if not empty_boxes:
+            return
 
-                x2 = x1 + cell_size
-                y2 = y1 + cell_size
+        box = random.choice(empty_boxes)
 
-                box = self.canvas.create_rectangle(
-                    x1, y1, x2, y2, outline="black", fill="white", width=2
-                )
+        self.computer_boxes.add(box)
 
-                self.canvas.tag_bind(box, "<Button-1>", lambda event, box=box, x1=x1, y1=y1, x2=x2, y2=y2:
-        self.box_clicked(event, box, x1, y1, x2, y2))
+        x1, y1, x2, y2 = self.box_coordinates[box]
 
-        self.canvas.pack(fill=BOTH, expand=1)
+        center_x = (x1 + x2) // 2
+        center_y = (y1 + y2) // 2
+
+        size = 30
+
+        self.canvas.create_oval(
+            center_x - size,
+            center_y - size,
+            center_x + size,
+            center_y + size,
+            width=4
+        )
+
 
 
 if __name__ == "__main__":
