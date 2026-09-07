660021e04e81dc68f4029b998004587ab6f0620c
Iahia <iahiaimon555@gmail.com>
2026-09-07 22:17:15 +0600

    Selection Shot

---
+++ selection_shot.py
@@ -0,0 +1,17 @@
+def selection(nums):
+    n = len(nums)
+    for i in range (n-1):
+        min_item = 100
+        min_index = -1
+        for j in range (i , n):
+            if nums[j] < min_item :
+                min_item = nums[j]
+                min_index = j
+        nums[min_index] = nums[i]
+        nums[i] = min_item
+
+list = [4 , 6 , 2 , 10 , 5 , 11 , 8]
+print(f"Before Shorting {list}")
+
+selection(list)
+print(f"After Sorting {list}")
